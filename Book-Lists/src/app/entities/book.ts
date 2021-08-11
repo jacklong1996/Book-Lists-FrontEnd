0ba910f0bf8dc2aa9391629bb005ae3b3d9fbc14
jacklong1996 <88209304+jacklong1996@users.noreply.github.com>
2021-08-10 20:21:20 -0500

    Added entity function

--- Book-Lists/src/app/entities/book.ts
+++ Book-Lists/src/app/entities/book.ts
@@ -1,3 +1,5 @@
+import { stringify } from "@angular/compiler/src/util"
+
 export class Book {
     constructor(
         public id: number, 
@@ -10,4 +12,16 @@ export class Book {
         public date: string, 
         public cover: string
     ) {}
+
+    getGenres(): string {
+        let returnStr = "";
+        for (let i = 0; i < this.genre.length; i++) {
+            if (i+1 == this.genre.length) {
+                returnStr += this.genre[i];
+            } else {
+                returnStr += this.genre[i] + ", ";
+            }
+        }
+        return returnStr;
+    }
 }
